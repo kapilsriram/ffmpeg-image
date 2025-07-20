6cbe058247bf3f7ef4a1282d8af6111f88572355
Kapil <kapilsriram@gmail.com>
2025-07-20 20:52:47 +0530

    files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, send_file
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/merge', methods=['POST'])
+def merge_video_image():
+    video = request.files['video']
+    image = request.files['image']
+
+    video.save('input.mp4')
+    image.save('input.png')
+
+    output = 'output.mp4'
+
+    cmd = [
+        'ffmpeg', '-i', 'input.mp4', '-i', 'input.png',
+        '-filter_complex', '[0:v][1:v] overlay=10:10',
+        '-codec:a', 'copy', output
+    ]
+
+    subprocess.run(cmd, check=True)
+    return send_file(output, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
