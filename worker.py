d4fa2ad68a4f0cea48db7ad23e697d3c86fe6a34
Kapil <kapilsriram@gmail.com>
2025-07-20 22:17:06 +0530

    files

--- worker.py
+++ worker.py
@@ -1,15 +1,12 @@
 from rq import Worker, Queue
-from rq.connection import Connection
-import redis
+from redis import Redis
 import os
 
 listen = ['default']
-
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
-conn = redis.from_url(redis_url)
+conn = Redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+    worker = Worker(queues=[Queue(name, connection=conn) for name in listen])
+    worker.work()
